3f3be84cc61d1889ba42af687090280ade94b421
erin petra sofiya moon <marshale@deshawresearch.com>
2025-10-08 17:43:10 -0400

    zsh/prompt: git branchname, dirtiness

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -11,7 +11,20 @@ function p_colored_path() {
 
 function p_ssh_markings() {
 	if [[ -n "${SSH_TTY}" ]]; then
-		echo "$(hostname -s)"
+		echo " $(hostname -s)"
+	fi
+}
+
+function p_git_markings() {
+	[[ "${MOON_DISABLE_GIT_PROMPT}" ]] && exit
+	git rev-parse --is-inside-work-tree >/dev/null 2>&1
+	if [[ "$?" = 0 ]]; then
+		local dirty="$(git status --porcelain)"
+		local branch="$(git rev-parse --abbrev-ref HEAD)"
+		if [[ "${branch}" = 'HEAD' ]]; then
+			branch="dtch@$(git rev-parse --short=8 HEAD)"
+		fi
+		printf " (${branch}%%F{red}${dirty:+*}%%f)"
 	fi
 }
 
@@ -19,7 +32,7 @@ function p_sigil() {
 	echo "%F{blue}λ%f"
 }
 
-prompt='$(p_sigil) $(p_colored_path) $(p_ssh_markings)
+prompt='$(p_sigil) $(p_colored_path)$(p_git_markings)$(p_ssh_markings)
 '
 if [[ -n "${ZSH_ITERM2_ACTIVE}" ]]; then
 	prompt+='%{$(iterm2_prompt_mark)%}'
